3fc21bd75141858b60ed9f95efbebe1cda450755
Tamunokorite Briggs <obriggs03@gmail.com>
2021-07-19 22:15:47 +0100

    Deploying backend

--- backend/startvest_api/settings.py
+++ backend/startvest_api/settings.py
@@ -16,7 +16,6 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -175,3 +174,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
+django_heroku.settings(locals())
